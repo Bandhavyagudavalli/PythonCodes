5d2951a0c0650c5a62695680ecc03215b2fd8561
GUDAVALLI BANDHAVYA <gudavallibandhavya@1130>
2025-05-21 22:11:03 +0530

    greatestoftwonnum.py

---
+++ greatestoftwonum.py
@@ -0,0 +1,9 @@
+n1=int(input("enter first number:"))
+n2=int(input("enter second number:"))
+if (n1>n2):
+    print ("n1 is greatest")
+elif ("n2>n1"):
+    print ("n2 is greatest")
+else:
+    print ("equal")
+
